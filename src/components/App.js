57037fcab9b55619c30e8e15d7ad5a06836a2cb7
Vikas Bhandari <143922604+Vikaskumarrr@users.noreply.github.com>
2025-01-02 21:05:42 +0000

    Added Solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { includes } from "cypress/types/lodash";
 
 const App = () => {
+  const [count , setCount ] = useState(0) 
+    const increment = ()=>{ 
+      setCount(count + 1); 
+    }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button Clicked {count} times</p>
+        <button onClick={increment}>Click me</button>
     </div>
   )
 }
